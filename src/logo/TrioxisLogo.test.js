310f25c05d7892f496a6039f7350e7166b5916bd
Sputnik <inquiry@dermah.com>
2017-10-27 11:07:58 +1100

    Add a test to check unique gradient ids are being used

--- src/logo/TrioxisLogo.test.js
+++ src/logo/TrioxisLogo.test.js
@@ -2,12 +2,27 @@ import React from 'react';
 import { mount } from 'enzyme';
 import {TrioxisLogo} from './';
 
-jest.mock('react-jss',()=>{
-  jest.spyOn(global.Math,'random').mockImplementation(()=>0.42);
+jest.mock('react-jss', () => {
+  let increment = 0.42
+  jest.spyOn(global.Math,'random').mockImplementation(() => increment += 0.01);
   return jest.requireActual('react-jss')
 })
 
-test('Logo should render as snapshot defines', () => {
-  const tree = mount(<TrioxisLogo />)
-  expect(tree).toMatchSnapshot()
-});
+describe('Logo', () => {
+  it('should render as snapshot defines', () => {
+    const tree = mount(<TrioxisLogo />)
+    expect(tree).toMatchSnapshot()
+  });
+
+  it('should render unique gradient ids to prevent clashes if there are multiple on the page', () => {
+    const tree = mount(
+      <div>
+        <TrioxisLogo />
+        <TrioxisLogo type={TrioxisLogo.Types.YellowRed} />
+      </div>
+    )
+
+    expect(tree.find('linearGradient').get(0).props.id)
+    .not.toBe(tree.find('linearGradient').get(1).props.id)
+  });
+})
