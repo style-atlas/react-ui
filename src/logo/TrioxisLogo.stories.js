23934298234aecc13f4826449db23967b4d46d64
Jack Davis <jdavis@trioxis.com>
2018-01-12 11:33:36 +1100

    Add documentation to logo

--- src/logo/TrioxisLogo.stories.js
+++ src/logo/TrioxisLogo.stories.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withInfo } from '@storybook/addon-info';
 
 import { StyleAtlasLogo } from './';
 
-storiesOf('StyleAtlasLogo', module)
-  .add('200x200', () => <StyleAtlasLogo width={200}/>)
-  .add('800x800', () => <StyleAtlasLogo width={800}/>)
+storiesOf('Logo', module)
+  .add('200x200', withInfo(``)
+    (() => <StyleAtlasLogo width={200}/>))
+  .add('800x800', withInfo(``)
+    (() => <StyleAtlasLogo width={800}/>))
