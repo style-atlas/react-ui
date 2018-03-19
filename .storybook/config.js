fbb835ee36c5dddcb0625e47ac2d7a3610e8da2c
Jack Davis <jdavis@trioxis.com>
2018-03-19 19:53:50 +0000

    Fix issue where tsx stories weren't detected

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@storybook/react';
 
-const req = require.context('../src', true, /\.stories\.js$/)
+const req = require.context('../src', true, /\.stories\.(ts|js)x?$/)
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename))
